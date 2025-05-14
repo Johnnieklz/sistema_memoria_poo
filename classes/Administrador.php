9d7224f4df8798c826a077f3da8c74942f68f370
Felipezxc <felipe.miranda377@gmail.com>
2025-05-14 20:14:04 -0300

    update administrador.php

--- classes/Administrador.php
+++ classes/Administrador.php
@@ -1,5 +1,24 @@
 <?php
-class Administrador extends Usuario {
+class Administrador extends Usuario 
     // Classe derivada
+        private $tipo;
+        private $id;
+        private $data_cadrastro;
+        private $idioma;
+        private $tema;
+        public function __construct($nome, $email, $senha, $tipo, $id, $data_cadrastro, $idioma, $tema){
+            $this->name = $nome;
+            $this->email = $email;
+            $this->senha = $senha;
+            $this->tipo = $tipo;
+            $this->id = $id;;
+            $this->data_cadrastro = $data_cadrastro;
+            $this->idioma = $idioma;
+            $this->tema = $tema;
+
+        }
+
+
 }
 ?>
+             
